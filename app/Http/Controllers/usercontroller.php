79b2541e3374db4562e30722cf3cb05a7f094c0e
NimmiKChandran27 <mca536@rajagiri.edu>
2021-05-27 14:57:11 +0530

    project

--- app/Http/Controllers/usercontroller.php
+++ app/Http/Controllers/usercontroller.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
-use\App\Models\User;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticationUsers;
  
 class usercontroller extends Controller
